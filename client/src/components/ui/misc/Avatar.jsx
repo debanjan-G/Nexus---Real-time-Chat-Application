2c8bd7c36eb8cdc21a2947acea0957573da9ad76
Debanjan <debanjanghosal10@gmail.com>
2024-10-24 22:42:12 +0530

    Fixed minor design issue in Chat Page Header

--- client/src/components/ui/misc/Avatar.jsx
+++ client/src/components/ui/misc/Avatar.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 
 const Avatar = ({ name, image }) => {
-  console.log(name);
-
   // Check if name is valid and split it to get initials
   const initials =
     name && name.split(" ").length > 1
@@ -13,13 +11,19 @@ const Avatar = ({ name, image }) => {
       : ""; // Fallback to first letter of the name if single name or empty
 
   return (
-    <div className="bg-sky-400 rounded-full p-2">
-      {/* {image ? (
-        <img src={image} alt={`${name}'s avatar`} className="rounded-full" />
-      ) : ( */}
-      <span className="text-white font-bold">{initials}</span>
-      {/* )} */}
-    </div>
+    <>
+      {image ? (
+        <img
+          src={image}
+          alt={`${name}'s avatar`}
+          className="rounded-full h-10"
+        />
+      ) : (
+        <div className="bg-sky-400 rounded-full p-2">
+          <span className="text-white font-bold">{initials}</span>
+        </div>
+      )}
+    </>
   );
 };
 
