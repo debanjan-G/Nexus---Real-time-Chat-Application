c0bfab1bb968b4550ab4926e22bfb94383ba8f70
Debanjan <debanjanghosal10@gmail.com>
2024-10-26 11:44:33 +0530

    Created client-side functionality to search for users and fetch them when inside create Group form

--- client/src/components/ui/misc/CreateGroupModal.jsx
+++ client/src/components/ui/misc/CreateGroupModal.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogBackdrop,
@@ -8,18 +8,67 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 import { Field, Input } from "@headlessui/react";
-
 import { IoIosCreate } from "react-icons/io";
+import { ChatState } from "../../context/ChatProvider";
+import Spinner from "../Spinner";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import { IoMdSearch } from "react-icons/io";
 
 export default function CreateGroupModal({ open, setOpen }) {
+  // form data states
+  const [groupName, setGroupName] = useState("");
+  const [groupMembers, setGroupMembers] = useState([]);
+
+  // search users functionality
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  //Loading State
+  const [loading, setLoading] = useState(false);
+
+  // context API
+  const { chats, setChats, user } = ChatState();
+
+  // toast (notifications)
+  const notify = (msg) => {
+    toast(msg);
+  };
+
+  // Function to create the group chat
   const createGroupChat = (e) => {
     console.log("Creating group chat...");
 
     e.preventDefault();
   };
 
+  const searchUsers = async () => {
+    if (!query) return;
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `http://localhost:8080/api/user?search=${query}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      console.log(response.data);
+
+      setSearchResults(response.data);
+    } catch (error) {
+      console.log("ERROR: ", error);
+      notify("User Not Found❗");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <Toaster />
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
@@ -47,18 +96,29 @@ export default function CreateGroupModal({ open, setOpen }) {
             >
               <Field className="w-full">
                 <Input
+                  value={groupName}
+                  onChange={(e) => setGroupName(e.target.value)}
+                  required
                   className="outline outline-slate-200 mt-3 block w-full rounded-lg border-none bg-slate-100 py-1.5 px-3 text-sm/6 text-slate-800
                     focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
                   placeholder="Group Chat Name"
                 />
               </Field>
-              <Field className="w-full">
+              <Field className="w-full flex items-center gap-2">
                 <Input
-                  className="outline outline-slate-200 mt-3 block w-full rounded-lg border-none bg-slate-100 py-1.5 px-3 text-sm/6 text-slate-800
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  required
+                  className="outline outline-slate-200 my-3 block w-full rounded-lg border-none bg-slate-100 py-1.5 px-3 text-sm/6 text-slate-800
                     focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
                   placeholder="Add users (eg: Arthur, Kenny, Angela)"
                 />
+                <IoMdSearch
+                  onClick={searchUsers}
+                  className=" text-3xl hover:text-green-700 duration-200 transition-all cursor-pointer"
+                />
               </Field>
+              {loading && <Spinner />}
 
               <div className=" px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 gap-4">
                 <button
